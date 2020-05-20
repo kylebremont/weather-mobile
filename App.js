6668c8d05dad004e7529398f148701f38b0ccb75
Kyle Bremont <kylebremont@gmail.com>
2020-05-20 14:30:43 -0600

    Added city prediction

--- App.js
+++ App.js
@@ -9,7 +9,10 @@ import {
   Text,
   StatusBar,
   TextInput,
+  FlatList,
+  TouchableHighlight,
 } from 'react-native';
+import { googleKey } from './config';
 
 
 class App extends Component {
@@ -18,16 +21,33 @@ class App extends Component {
 
     this.state = {
       city: 'Anchorage',
+      url: 'https://maps.googleapis.com/maps/api/place/autocomplete/json?',
+      predict_data: [],
     }
 
     this.updateCity = this.updateCity.bind(this);
+    this.PredictCity = this.PredictCity.bind(this);
 
   }
 
   updateCity(city) {
     if (city !== "") {
+      city = city.slice(0, city.indexOf('USA')-2).replace(' ', '');
       this.setState({ city: city.substring(0, city.indexOf(',')) }, () => this.refs.weather.ChangeCity(city))
-    }
+    }    
+  }
+
+  PredictCity(search) {
+    fetch(`${this.state.url}input=${search}&types=(cities)&key=${googleKey}`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({predict_data: result.predictions});
+                },
+                (error) => {
+                    this.setState({error});
+                }
+            )
   }
 
   render() {
@@ -37,14 +57,41 @@ class App extends Component {
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.container}>
-          <ScrollView style={styles.scrollView}>
+          {this.state.predict_data.length === 0 && (
             <TextInput
-              style={styles.searchBox}
-              placeholder="Enter city as 'Example,EX'"
+              style={styles.searchBoxBeforeSearch}
+              placeholder="Enter city"
               placeholderTextColor="grey"
-              onSubmitEditing={(event) => this.updateCity(event.nativeEvent.text)}
+              onChangeText={(search) => this.PredictCity(search)}
             />
-            <Text style={styles.cityName}>{this.state.city}</Text>
+          )}
+          {this.state.predict_data.length !== 0 && (
+            <>
+              <TextInput
+                style={styles.searchBoxAfterSearch}
+                placeholder="Enter city"
+                placeholderTextColor="grey"
+                onChangeText={(search) => this.PredictCity(search)}
+              />
+              <FlatList
+              data={this.state.predict_data}
+              renderItem={({item, index, separators}) => (
+                <TouchableHighlight
+                  style={styles.searchList}
+                  onPress={() => this.updateCity(item.description)}
+                  onShowUnderlay={separators.highlight}
+                  onHideUnderlay={separators.unhighlight}
+                >
+                  <Text style={styles.searchItem}>
+                    {item.description}
+                  </Text>
+                </TouchableHighlight>
+              )}
+              />
+            </>
+          )}
+          <Text style={styles.cityName}>{this.state.city}</Text>
+          <ScrollView style={styles.scrollView}>
             <Weather ref="weather"></Weather>
           </ScrollView>
         </SafeAreaView>
@@ -60,12 +107,40 @@ const styles = StyleSheet.create({
     backgroundColor: "#4798c5"
   },
 
-  searchBox: {
+  searchList: {
+    marginLeft: 41,
+    marginRight: 41,
+  },
+
+  searchItem: {
+    backgroundColor: 'whitesmoke',
+    color: 'black',
+    padding: 10,
+  },
+
+  searchBoxAfterSearch: {
     height: 40,
-    marginLeft: 15,
-    marginRight: 15,
+    marginLeft: 40,
+    marginRight: 40,
     marginTop: 20,
-    backgroundColor: "white",
+    textAlign: 'center',
+    color: 'black',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: "whitesmoke",
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+
+  searchBoxBeforeSearch: {
+    height: 40,
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 20,
+    textAlign: 'center',
+    color: 'black',
+    borderRadius: 20,
+    backgroundColor: "whitesmoke",
     borderColor: 'gray',
     borderWidth: 1,
   },
